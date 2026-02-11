2bfecedc8c26a807e33a4814994feb1b67ec0490
Syed Muqaddam Abbas <smmehdi@SMs-MacBook-Air.local>
2026-02-12 02:04:01 +0530

    New web development lecture folder

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+// import generateName from 'sillyname';
+
+// var sillyName = generateName();
+// console.log(`my name is ${sillyName}`);
+
+import {randomSuperhero} from 'superheroes';
+
+var superName = randomSuperhero();
+console.log(`my name is ${superName}`);
