6ec74f0286a61165bd46da9b7b673e78704eda31
Syed Muqaddam Abbas <muqaddam257@gmail.com>
2026-02-12 06:42:54 +0530

    Fixed nested repo

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,15 @@
 
-$(document).keydown( function(event){
-    $("h1").text(event.text);
+$("h1").css("color", "red");
+
+
+// $(document).keydown( function(event){
+//     $("h1").text(event.key);
+// })
+
+// $("button").click( function(){
+//     $("h1").slideToggle();
+// })
+
+$("button").click( function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.7});
 })
